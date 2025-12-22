ee6fcdfe9d827f8a6907758139928b83b760287c
1155381 <donavanriddick08@gmail.com>
2025-12-22 14:51:43 -0500

    finished my shared preference and design of second xml

--- app/src/main/java/org/baltimorecityschools/quizlgyrapp/MainActivity.java
+++ app/src/main/java/org/baltimorecityschools/quizlgyrapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -17,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences openPreference;
     final  String sharedPreferencesFile = "org.baltimorecityschools.donavansharepreferences.sp";
     final String FIRST_OPEN_KEY = "FIRSTOPEN";
+    final String USERNAME_KEY = "USERNAME";
 
     Button doneBTN;
     EditText enterUsername;
+    TextView usernameTV;
     String username;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         enterUsername = (EditText) findViewById(R.id.setUsername);
         doneBTN = (Button) findViewById(R.id.donebut);
+        usernameTV = findViewById(R.id.usernametextview);
+        username = "User";
         openPreference = getSharedPreferences(sharedPreferencesFile, MODE_PRIVATE);
+        editor = openPreference.edit();
+
 
         if (openPreference.getBoolean(FIRST_OPEN_KEY, true)){
             enterUsername.setVisibility(View.VISIBLE);
-            SharedPreferences.Editor editor = openPreference.edit();
             editor.putBoolean(FIRST_OPEN_KEY, false);
             editor.apply();
 
@@ -40,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             enterUsername.setVisibility(View.GONE);
+            username = openPreference.getString(USERNAME_KEY, "User");
+            usernameTV.setText("Welcome back " + username);
+            usernameTV.setVisibility(View.VISIBLE);
             doneBTN.setText("START");
 
         }
@@ -53,9 +63,11 @@ public class MainActivity extends AppCompatActivity {
                     username = "";
                     enterUsername.setText(username);
                 } else {
+                    username = enterUsername.getText().toString();
+                    editor.putString(USERNAME_KEY, username);
+                    editor.apply();
                     Intent leaveIntent;
                     leaveIntent = new Intent(MainActivity.this, QuizSelectActivity.class);
-                    leaveIntent.putExtra("usernameExtra", username);
                     startActivity(leaveIntent);
                 }
 
--- app/src/main/java/org/baltimorecityschools/quizlgyrapp/QuizSelectActivity.java
+++ app/src/main/java/org/baltimorecityschools/quizlgyrapp/QuizSelectActivity.java
@@ -1,7 +1,9 @@
 package org.baltimorecityschools.quizlgyrapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +11,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizSelectActivity extends AppCompatActivity {
 
+    final  String sharedPreferencesFile = "org.baltimorecityschools.donavansharepreferences.sp";
+    final String USERNAME_KEY = "USERNAME";
+    SharedPreferences openPreference;
     public String username;
+    TextView usernameTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_select);
-        Intent intent = getIntent();
-        username = intent.getStringExtra(username);
+        usernameTV = findViewById(R.id.usernametext);
+        openPreference = getSharedPreferences(sharedPreferencesFile, MODE_PRIVATE);
+        username = openPreference.getString(USERNAME_KEY, "User");
+        usernameTV.setText(username);
+
+
 
     }
 }
